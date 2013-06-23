6dd98059c86b5ce79675107a049a599a2834994c
Sergi Tur Badenas <sergitur@ebretic.com>
2013-06-23 17:19:35 +0200

    Added fields to show

--- application/config/inventory.php
+++ application/config/inventory.php
@@ -9,7 +9,7 @@
 | 
 | Example: Institut de l'Ebre
 */
-$config['institution_name']	= "Institut Montsià";
+$config['institution_name']	= "Institut de l'Ebre";
 
 /*
 |--------------------------------------------------------------------------
@@ -135,6 +135,395 @@ $config['realms'] = "mysql,ldap";
 */
 $config['default_realm'] = "ldap";
 
+/*
+|--------------------------------------------------------------------------
+| GROUP WITH READONLY ROLE
+|--------------------------------------------------------------------------
+|
+| Groups with readonly acces to app
+| 
+| Example: inventory_readonly
+*/
+$config['readonly_group'] = "inventory_readonly";
+
+
+/*
+|--------------------------------------------------------------------------
+| GROUP WITH ADMIN ROLE
+|--------------------------------------------------------------------------
+|
+| Groups with admin role
+| 
+| Example: inventory_readonly
+*/
+$config['admin_group'] = "inventory_admin";
+
+
+// DEFAULT FIELDS
+  
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  inventory_object
+|--------------------------------------------------------------------------
+* 'inventory_objectId',
+    'publicId',  
+    'externalID',
+    'externalIDType',
+    'name',
+    'shortName',  
+    'description',
+    'materialId', 
+    'brandId',  
+    'modelId',  
+    'entryDate',
+    'manualEntryDate',
+    'last_update',
+    'manualLast_update',
+    'creationUserId',  
+    'lastupdateUserId',
+    'location',
+    'quantityInStock',
+    'price',
+    'moneySourceId',
+    'providerId',
+    'preservationState',
+    'markedForDeletion',
+    'markedForDeletionDate',
+    'file_url'
+* 
+*/
+$config['default_fields_table_inventory_object'] = array (
+    'quantityInStock',
+    'shortName', 
+    'publicId',  
+    'externalID', 
+    'materialId',
+    'brandId',
+    'modelId',
+    'OwnerOrganizationalUnit',
+    'entryDate',  
+    'last_update',
+    'lastupdateUserId',
+    'location',
+    'file_url'
+    );
+  
+/*
+
+
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  model
+|--------------------------------------------------------------------------
+*   'modelId',
+    'name',
+    'shortName',  
+    'description',      
+    'entryDate',       
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_model'] = array (
+    'name',
+    'shortName',  
+    'description',         
+    'entryDate',       
+    'last_update',      
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+    
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  material
+|--------------------------------------------------------------------------
+*   'materialId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_material'] = array (
+    'name',
+    'shortName',           
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+    
+  
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  externalIDType
+|--------------------------------------------------------------------------
+*   'externalIDTypeID',
+    'name',
+    'shortName',       
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_externalIDType'] = array (
+    'name',
+    'shortName',       
+    'description',      
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+  
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  organizational_unit
+|--------------------------------------------------------------------------
+*   'organizational_unitId',
+    'externalCode',
+    'name', 
+    'shortName',       
+    'description',     
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'parentLocation',   
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_organizational_unit'] = array (
+    'externalCode',
+    'name',
+    'shortName',       
+    'description',      
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+    
+    
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  location
+|--------------------------------------------------------------------------
+*   'locationId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'parentLocation',   
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_location'] = array (
+    'name',
+    'shortName',           
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'parentLocation',      
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+    
+  
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  material
+|--------------------------------------------------------------------------
+*   'materialId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_material'] = array (
+    'name',
+    'shortName',           
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+    
+
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  brand
+|--------------------------------------------------------------------------
+*   'brandId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_brand'] = array (
+    'name',
+    'shortName',           
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+
+
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  provider
+|--------------------------------------------------------------------------
+*   'providerId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_provider'] = array (
+    'name',
+    'shortName',           
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+
+
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  money_source
+|--------------------------------------------------------------------------
+*   'moneySourceId',
+    'name',
+    'shortName',        
+    'description',      
+    'entryDate',        
+    'last_update',      
+    'creationUserId',   
+    'lastupdateUserId', 
+    'markedForDeletion',
+    'markedForDeletionDate',
+* 
+*/
+$config['default_fields_table_money_source'] = array (
+    'name',
+    'shortName',        
+    'description',         
+    'entryDate',           
+    'last_update',         
+    'creationUserId',      
+    'lastupdateUserId',    
+    'markedForDeletion',   
+    'markedForDeletionDate'
+    );
+
+
+/*       
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  users
+|--------------------------------------------------------------------------
+*   'id',
+    'ip_address',
+    'username',  
+    'password',  
+    'salt',
+    'email',
+    'activation_code',
+    'forgotten_password_code',
+    'forgotten_password_time',
+    'remember_code',
+    'created_on',   
+    'last_login',   
+    'active',
+    'first_name',
+    'last_name', 
+    'company',   
+    'phone',     
+* 
+*/
+
+$config['default_fields_table_users'] = array (
+    'username',   
+    'email',
+    'created_on',   
+    'last_login',
+    'active',
+    'first_name',
+    'last_name',
+    'company',
+    'phone',
+    );
+    
+/*
+|--------------------------------------------------------------------------
+| DEFAULT FIELDS TABLE  groups
+|--------------------------------------------------------------------------
+*   'id',
+    'name',
+    'description'
+* 
+*/
+$config['default_fields_table_groups'] = array (
+    'name',
+    'description'   
+    ); 
 
 /* End of file inventory.php */
 /* Location: ./application/config/inventory.php */
