743a41939c41c680897e53dc14b5907d3a287240
Sergi Tur Badenas <sergitur@ebretic.com>
2013-06-25 13:28:24 +0200

    Added ci_sessions. Sessions are now handled by database no cookies

--- inventory_only_structure.sql
+++ inventory_only_structure.sql
@@ -341,3 +341,22 @@ CREATE TABLE IF NOT EXISTS `model` (
   PRIMARY KEY (`modelId`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Base de dades: `ebreinventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
