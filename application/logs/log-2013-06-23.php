a09cdff1f5b5c2d8ccdd31efee781a0dd3c3d995
Sergi Tur Badenas <sergitur@ebretic.com>
2013-06-27 16:36:08 +0200

    adssadk

---
+++ application/logs/log-2013-06-23.php
@@ -0,0 +1,162 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-06-23 10:33:22 --> Config Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Hooks Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Utf8 Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> UTF-8 Support Enabled
+DEBUG - 2013-06-23 10:33:22 --> URI Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Router Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Output Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Security Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Input Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-06-23 10:33:22 --> Language Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Loader Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Controller Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Config file loaded: application/config/ion_auth.php
+DEBUG - 2013-06-23 10:33:22 --> Email Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Language file loaded: language/catalan/ion_auth_lang.php
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: cookie_helper
+DEBUG - 2013-06-23 10:33:22 --> Session Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: string_helper
+DEBUG - 2013-06-23 10:33:22 --> Session routines successfully run
+DEBUG - 2013-06-23 10:33:22 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Database Driver Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: date_helper
+DEBUG - 2013-06-23 10:33:22 --> Language file loaded: language/catalan/auth_ldap_lang.php
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: language_helper
+DEBUG - 2013-06-23 10:33:22 --> Auth_Ldap initialization ...
+DEBUG - 2013-06-23 10:33:22 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2013-06-23 10:33:22 --> Config file loaded: application/config/auth_ldap.php
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: form_helper
+DEBUG - 2013-06-23 10:33:22 --> Form Validation Class Initialized
+DEBUG - 2013-06-23 10:33:22 --> Helper loaded: url_helper
+DEBUG - 2013-06-23 10:33:22 --> Language file loaded: language/catalan/auth_lang.php
+DEBUG - 2013-06-23 10:33:22 --> Config file loaded: application/config/inventory.php
+DEBUG - 2013-06-23 10:33:22 --> Language file loaded: language/catalan/inventory_lang.php
+ERROR - 2013-06-23 10:33:22 --> Severity: Notice  --> A session had already been started - ignoring session_start() /home/sergi/GITHUB/ebre-inventory/application/controllers/inventory_auth.php 56
+DEBUG - 2013-06-23 10:33:22 --> Language file loaded: language/catalan/form_validation_lang.php
+DEBUG - 2013-06-23 10:33:23 --> S'ha realitzar el bind al servidor Ldap correctament. Realitzant la cerca del dn...sergitur
+INFO  - 2013-06-23 10:33:24 --> sergitur no té cap rol assignat.
+ERROR - 2013-06-23 10:33:24 --> Could not find the language line "successfully_authenticated_but_no_role"
+DEBUG - 2013-06-23 10:33:41 --> Config Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Hooks Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Utf8 Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> UTF-8 Support Enabled
+DEBUG - 2013-06-23 10:33:41 --> URI Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Router Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Output Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Security Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Input Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-06-23 10:33:41 --> Language Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Loader Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Controller Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Config file loaded: application/config/ion_auth.php
+DEBUG - 2013-06-23 10:33:41 --> Email Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Language file loaded: language/catalan/ion_auth_lang.php
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: cookie_helper
+DEBUG - 2013-06-23 10:33:41 --> Session Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: string_helper
+DEBUG - 2013-06-23 10:33:41 --> Session routines successfully run
+DEBUG - 2013-06-23 10:33:41 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Database Driver Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: date_helper
+DEBUG - 2013-06-23 10:33:41 --> Language file loaded: language/catalan/auth_ldap_lang.php
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: language_helper
+DEBUG - 2013-06-23 10:33:41 --> Auth_Ldap initialization ...
+DEBUG - 2013-06-23 10:33:41 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2013-06-23 10:33:41 --> Config file loaded: application/config/auth_ldap.php
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: form_helper
+DEBUG - 2013-06-23 10:33:41 --> Form Validation Class Initialized
+DEBUG - 2013-06-23 10:33:41 --> Helper loaded: url_helper
+DEBUG - 2013-06-23 10:33:41 --> Language file loaded: language/catalan/auth_lang.php
+DEBUG - 2013-06-23 10:33:41 --> Config file loaded: application/config/inventory.php
+DEBUG - 2013-06-23 10:33:41 --> Language file loaded: language/catalan/inventory_lang.php
+ERROR - 2013-06-23 10:33:41 --> Severity: Notice  --> A session had already been started - ignoring session_start() /home/sergi/GITHUB/ebre-inventory/application/controllers/inventory_auth.php 56
+DEBUG - 2013-06-23 10:33:41 --> Language file loaded: language/catalan/form_validation_lang.php
+DEBUG - 2013-06-23 10:33:41 --> S'ha realitzar el bind al servidor Ldap correctament. Realitzant la cerca del dn...sergitur
+INFO  - 2013-06-23 10:33:42 --> sergitur no té cap rol assignat.
+ERROR - 2013-06-23 10:33:42 --> Could not find the language line "successfully_authenticated_but_no_role"
+DEBUG - 2013-06-23 10:33:49 --> Config Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Hooks Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Utf8 Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> UTF-8 Support Enabled
+DEBUG - 2013-06-23 10:33:49 --> URI Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Router Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Output Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Security Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Input Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-06-23 10:33:49 --> Language Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Loader Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Controller Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Config file loaded: application/config/ion_auth.php
+DEBUG - 2013-06-23 10:33:49 --> Email Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Language file loaded: language/catalan/ion_auth_lang.php
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: cookie_helper
+DEBUG - 2013-06-23 10:33:49 --> Session Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: string_helper
+DEBUG - 2013-06-23 10:33:49 --> Session routines successfully run
+DEBUG - 2013-06-23 10:33:49 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Model Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Database Driver Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: date_helper
+DEBUG - 2013-06-23 10:33:49 --> Language file loaded: language/catalan/auth_ldap_lang.php
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: language_helper
+DEBUG - 2013-06-23 10:33:49 --> Auth_Ldap initialization ...
+DEBUG - 2013-06-23 10:33:49 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2013-06-23 10:33:49 --> Config file loaded: application/config/auth_ldap.php
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: form_helper
+DEBUG - 2013-06-23 10:33:49 --> Form Validation Class Initialized
+DEBUG - 2013-06-23 10:33:49 --> Helper loaded: url_helper
+DEBUG - 2013-06-23 10:33:49 --> Language file loaded: language/catalan/auth_lang.php
+DEBUG - 2013-06-23 10:33:49 --> Config file loaded: application/config/inventory.php
+DEBUG - 2013-06-23 10:33:49 --> Language file loaded: language/catalan/inventory_lang.php
+ERROR - 2013-06-23 10:33:49 --> Severity: Notice  --> A session had already been started - ignoring session_start() /home/sergi/GITHUB/ebre-inventory/application/controllers/inventory_auth.php 56
+DEBUG - 2013-06-23 10:33:49 --> Language file loaded: language/catalan/form_validation_lang.php
+DEBUG - 2013-06-23 10:33:49 --> S'ha realitzar el bind al servidor Ldap correctament. Realitzant la cerca del dn...sergitur
+INFO  - 2013-06-23 10:33:50 --> sergitur no té cap rol assignat.
+ERROR - 2013-06-23 10:33:50 --> Could not find the language line "successfully_authenticated_but_no_role"
+DEBUG - 2013-06-23 10:34:37 --> Config Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Hooks Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Utf8 Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> UTF-8 Support Enabled
+DEBUG - 2013-06-23 10:34:37 --> URI Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Router Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Output Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Security Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Input Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-06-23 10:34:37 --> Language Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Loader Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Controller Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Config file loaded: application/config/ion_auth.php
+DEBUG - 2013-06-23 10:34:37 --> Email Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Language file loaded: language/catalan/ion_auth_lang.php
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: cookie_helper
+DEBUG - 2013-06-23 10:34:37 --> Session Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: string_helper
+DEBUG - 2013-06-23 10:34:37 --> Session routines successfully run
+DEBUG - 2013-06-23 10:34:37 --> Model Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Model Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Database Driver Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: date_helper
+DEBUG - 2013-06-23 10:34:37 --> Language file loaded: language/catalan/auth_ldap_lang.php
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: language_helper
+DEBUG - 2013-06-23 10:34:37 --> Auth_Ldap initialization ...
+DEBUG - 2013-06-23 10:34:37 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2013-06-23 10:34:37 --> Config file loaded: application/config/auth_ldap.php
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: form_helper
+DEBUG - 2013-06-23 10:34:37 --> Form Validation Class Initialized
+DEBUG - 2013-06-23 10:34:37 --> Helper loaded: url_helper
+DEBUG - 2013-06-23 10:34:37 --> Language file loaded: language/catalan/auth_lang.php
+DEBUG - 2013-06-23 10:34:37 --> Config file loaded: application/config/inventory.php
+DEBUG - 2013-06-23 10:34:37 --> Language file loaded: language/catalan/inventory_lang.php
+ERROR - 2013-06-23 10:34:37 --> Severity: Notice  --> A session had already been started - ignoring session_start() /home/sergi/GITHUB/ebre-inventory/application/controllers/inventory_auth.php 56
+DEBUG - 2013-06-23 10:34:37 --> Language file loaded: language/catalan/form_validation_lang.php
+DEBUG - 2013-06-23 10:34:38 --> S'ha realitzar el bind al servidor Ldap correctament. Realitzant la cerca del dn...sergitur
+INFO  - 2013-06-23 10:34:38 --> sergitur no té cap rol assignat.
+ERROR - 2013-06-23 10:34:38 --> Could not find the language line "successfully_authenticated_but_no_role"
